58b0049e84431b0516319b53bfd4b8b5e6bd6ef3
htjaya <htjayawardana@gmail.com>
2020-11-16 01:08:19 +0530

    registration done

--- page/singup.js
+++ page/singup.js
@@ -64,6 +64,16 @@ UserRegistrationFunction = () =>{
 }
  
   render() {
+    const { navigate } = this.props.navigation;
+    state = {
+      language: 'java',
+
+    };
+
+ 
+//     this.state = {
+//       role: 'Director'
+//   }
     return (
  
         <SafeAreaView>
@@ -213,7 +223,7 @@ const styles = StyleSheet.create({
       flex: 1,
       backgroundColor: 'white',
       justifyContent: 'center',
-      
+     
     },
     inputs:{
       borderBottomWidth: 1,
